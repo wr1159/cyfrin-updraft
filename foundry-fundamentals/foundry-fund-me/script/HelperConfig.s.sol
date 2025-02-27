f07e749180a8d23e61ed943a4c4e377eda9b0167
wr1159 <chuweiron18@gmail.com>
2025-02-27 23:31:42 +0000

    Add helperConfig into FundMe

---
+++ foundry-fundamentals/foundry-fund-me/script/HelperConfig.s.sol
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.19;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelperConfig {
+    // If we are on a local Anvil, we deploy the mocks
+    // Else, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH/USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {}
+}
